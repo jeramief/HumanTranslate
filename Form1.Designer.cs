5913918679d63026ecd3d96e9582361e561d82f9
jeramief <109633173+jeramief@users.noreply.github.com>
2023-06-01 15:46:47 -0400

    Japanese translation quiz

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HumanTranslate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HumanTranslate.Properties.Resources.tinywow_text_to_image_25341873;
+            this.pictureBox1.Location = new System.Drawing.Point(428, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 98);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(281, 98);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(281, 98);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(465, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(281, 98);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(12, 12);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(363, 116);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "これは何を言いますか";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblQuestion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanTranslate
+{
+
+    public partial class Form1 : Form
+    {
+
+        // variables list for this quiz game
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            AskQuestion(questionNumber);
+
+            totalQuestions = 8;
+        }
+
+        private void ClickAnswerEvent(object sender, EventArgs e)
+        {
+
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+
+                score++;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                // work out the percentage here
+                percentage = (int)Math.Round((double)(100 * score) / totalQuestions);
+
+
+                MessageBox.Show("Quiz Ended" + Environment.NewLine +
+                                "You have answered " + score + " questions correcly" + Environment.NewLine +
+                                "Your total percentage is " + percentage + " %" + Environment.NewLine +
+                                "Click Ok to play again"
+
+                    );
+
+                score = 0;
+                questionNumber = 0;
+
+                AskQuestion(questionNumber);
+            }
+
+            questionNumber++;
+
+            AskQuestion(questionNumber);
+        }
+
+
+        private void AskQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+
+                case 1:
+
+                    lblQuestion.Text = "In English, what does " + "月曜日" + " mean?";
+
+                    button1.Text = "today";
+                    button2.Text = "Sunday";
+                    button3.Text = "purple";
+                    button4.Text = "Monday";
+
+                    correctAnswer = 4;
+
+                    break;
+                case 2:
+
+                    lblQuestion.Text = "In English, what does " + "高い" + " mean?";
+
+                    button1.Text = "like";
+                    button2.Text = "expensive";
+                    button3.Text = "brother";
+                    button4.Text = "cheap";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 3:
+
+                    lblQuestion.Text = "In Japanese, how do you say " + "friend" + "?";
+
+                    button1.Text = "友達";
+                    button2.Text = "野菜";
+                    button3.Text = "彼女";
+                    button4.Text = "話せ";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 4:
+
+                    lblQuestion.Text = "In English, how do you say " + "京都" + "?";
+
+                    button1.Text = "Tokyo";
+                    button2.Text = "Kyoto";
+                    button3.Text = "Oosaka";
+                    button4.Text = "Naomi";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 5:
+
+                    lblQuestion.Text = "In Japanese, what is " + "毎朝" + "?";
+
+                    button1.Text = "まいあさ";
+                    button2.Text = "おちゃ";
+                    button3.Text = "ごはん";
+                    button4.Text = "おくさん";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 6:
+
+                    lblQuestion.Text = "In Japanese, how do you say " + "grandchild" + "?";
+
+                    button1.Text = "窓";
+                    button2.Text = "買い";
+                    button3.Text = "孫";
+                    button4.Text = "元気";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 7:
+
+                    lblQuestion.Text = "In Japanese, how do you say" + "Goodnight" + "?";
+
+                    button1.Text = "おやすみなさい。";
+                    button2.Text = "こにちは";
+                    button3.Text = "おはようございます";
+                    button4.Text = "こんばんは";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 8:
+
+                    lblQuestion.Text = "In English, what does " + "百円" + "mean?";
+
+                    button1.Text = "dirty";
+                    button2.Text = "pull";
+                    button3.Text = "a hundred yen";
+                    button4.Text = "five dollars";
+
+                    correctAnswer = 3;
+
+                    break;
+            }
+
+        }
+
+    }
+}
